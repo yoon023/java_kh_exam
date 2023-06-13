fdc9e3099b2afc1ef3ad9bd38af03655283e030e
yoon023 <yoon200004@gmail.com>
2023-06-13 18:26:38 +0900

    .

--- 20230612_stream/src/file/HelloWorld.java
+++ 20230612_stream/src/file/HelloWorld.java
@@ -6,7 +6,7 @@ import java.io.IOException;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		try(FileReader fr = new FileReader("b.txt")){
+		try(FileReader fr = new FileReader("a.txt")){
 			int i;
 			while((i = fr.read()) != -1) {
 				System.out.println((char)i);
